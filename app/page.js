98191c4e773481ab535c3297ba88ace3a63067a0
bizarreuproar <emirys774@gmail.com>
2024-02-10 19:45:00 +0300

    Update videoChat.jsx and remove socket.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import VideoChat from "../components/videoChat";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="">
       <VideoChat />
     </main>
   );
--- components/videoChat.jsx
+++ components/videoChat.jsx
@@ -1,52 +1,155 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { io } from 'socket.io-client';
 import SimplePeer from 'simple-peer';
-import io from 'socket.io-client';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
-const socket = io('http://localhost:3000');
+const socket = io('http://localhost:3001');
 
 const VideoChat = () => {
-  const [peers, setPeers] = useState([]);
-  const [stream, setStream] = useState(null);
-  const [audioEnabled, setaudioEnable] = useState(true);
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
+  const [stream, setStream] = useState();
+  const [name, setName] = useState('');
+  const [call, setCall] = useState({});
+  const [me, setMe] = useState('');
+  const [idToCall, setIdToCall] = useState('');
+
+  const myVideo = useRef(null);
+  const userVideo = useRef(null);
+  const connectionRef = useRef(null);
 
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-      .then(mediaStream => {
-        setStream(mediaStream);
+      .then((currentStream) => {
+        setStream(currentStream);
 
-        const myPeer = new SimplePeer({ initiator: true, stream: mediaStream });
+        myVideo.current && (myVideo.current.srcObject = currentStream);
+      });
 
-        myPeer.on('signal', (signal) => {
-          socket.emit('offer', signal);
-        });
+    socket.on('me', (id) => { setMe(id) });
 
-        socket.on('offer', (signal) => {
-          const peer = new SimplePeer({ initiator: false, stream: mediaStream });
-          peer.signal(signal);
+    socket.on('callUser', ({ from, name: callerName, signal }) => {
+      setCall({ isReceivingCall: true, from, name: callerName, signal });
+    });
 
-          peer.on('signal', (answerSignal) => {
-            socket.emit('answer', answerSignal);
-          });
-
-          setPeers(prevPeers => [...prevPeers, peer]);
-        });
-
-        return () => {
-          myPeer.destroy();
-          peers.forEach((peer) => peer.destroy());
-        };
-      })
-      .catch(error => console.error('Error accessing media devices:', error));
   }, []);
 
+  const answerCall = () => {
+    setCallAccepted(true);
+
+    const peer = new SimplePeer({ initiator: false, trickle: false, stream });
+
+    peer.on('signal', (data) => {
+      socket.emit('answerCall', { signal: data, to: call.from });
+    });
+
+    peer.on('stream', (currentStream) => {
+      userVideo.current.srcObject = currentStream;
+    });
+
+    peer.signal(call.signal);
+
+    connectionRef.current = peer;
+  };
+
+  const callUser = (id) => {
+    const peer = new SimplePeer({ initiator: true, trickle: false, stream });
+
+    peer.on('signal', (data) => {
+      socket.emit('callUser', { userToCall: id, signalData: data, from: me, name });
+    });
+
+    peer.on('stream', (currentStream) => {
+      userVideo.current.srcObject = currentStream;
+    });
+
+    socket.on('callAccepted', (signal) => {
+      setCallAccepted(true);
+
+      peer.signal(signal);
+    });
+
+    connectionRef.current = peer;
+  };
+
+  const leaveCall = () => {
+    setCallEnded(true);
+
+    connectionRef.current && connectionRef.current.destroy();
+    window.location.reload();
+  };
+
   return (
-    <div>
-      {stream && <video ref={(ref) => ref && (ref.srcObject = stream)} autoPlay muted />}
-      {peers.map((peer, index) => (
-        <video key={index} ref={(ref) => ref && (ref.srcObject = peer.stream)} autoPlay></video>
-      ))}
-    </div>
+    <>
+      <div className="flex justify-center items-center">
+        {stream && (
+          <div className="border-2 border-black rounded-lg p-4 m-4">
+            <h5 >{name || 'Name'}</h5>
+            <video playsInline muted ref={myVideo} autoPlay className="w-64 md:w-80" />
+          </div>
+        )}
+        {call.isReceivingCall && !callAccepted && (
+          <div className="flex justify-around items-center">
+            <h1>{call.name} is calling:</h1>
+            <button onClick={answerCall} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none">
+              Answer
+            </button>
+          </div>
+        )}
+        {callAccepted && !callEnded && (
+          <div className="border-2 border-black rounded-lg p-4 m-4">
+            <h5 >{call.name || 'Name'}</h5>
+            <video playsInline ref={userVideo} autoPlay className="w-64 md:w-80" />
+          </div>
+        )}
+        <div className="w-full md:w-3/4 lg:w-1/2 xl:w-1/3 mx-auto mt-8">
+          <div className="p-8 border-2 border-black">
+            <div className="flex flex-col" noValidate autoComplete="off">
+              <div className="w-full">
+                {/* Account Info */}
+                <div className="p-4">
+                  <h6 className="text-lg mb-2">Account Info</h6>
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="border-2 border-gray-300 px-3 py-2 rounded-md w-full focus:outline-none focus:ring focus:ring-blue-200"
+                    placeholder="Name"
+                  />
+                  <CopyToClipboard text={me} className="mt-4 bg-blue-500 rounded-md outline-none">
+                    <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none">
+                      {me}
+                    </button>
+                  </CopyToClipboard>
+                </div>
+
+                {/* Make a call */}
+                <div className="p-4">
+                  <h6 className="text-lg mb-2">Make a call</h6>
+                  <input
+                    type="text"
+                    value={idToCall}
+                    onChange={(e) => setIdToCall(e.target.value)}
+                    className="border-2 border-gray-300 px-3 py-2 rounded-md w-full focus:outline-none focus:ring focus:ring-blue-200"
+                    placeholder="ID to call"
+                  />
+                  {callAccepted && !callEnded ? (
+                    <button onClick={leaveCall} className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 focus:outline-none mt-4">
+                      Hang Up
+                    </button>
+                  ) : (
+                    <button onClick={() => callUser(idToCall)} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none mt-4">
+                      Call
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div >
+    </>
   );
 };
 
