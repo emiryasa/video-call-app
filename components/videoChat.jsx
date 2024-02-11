ea13caf3e32e8682853727167ead77bbf023d8fb
bizarreuproar <emirys774@gmail.com>
2024-02-11 19:47:11 +0300

    Refactor Options component to include camera toggle

--- components/videoChat.jsx
+++ components/videoChat.jsx
@@ -3,9 +3,10 @@ import React, { useState, useRef, useEffect } from 'react';
 import { io } from 'socket.io-client';
 import SimplePeer from 'simple-peer';
 import CopyToClipboard from 'react-copy-to-clipboard';
+import Options from './Options'; // Options bileşenini import ettik
 
 const VideoChat = () => {
-  const [socket, setSocket] = useState(null); // Socket.IO bağlantısını saklamak
+  const [socket, setSocket] = useState(null);
   const [callAccepted, setCallAccepted] = useState(false);
   const [callEnded, setCallEnded] = useState(false);
   const [stream, setStream] = useState();
@@ -17,13 +18,11 @@ const VideoChat = () => {
   const myVideo = useRef(null);
   const userVideo = useRef(null);
   const connectionRef = useRef(null);
-  
+
   useEffect(() => {
-    // Socket.IO bağlantısını oluşturmak
     const newSocket = io('http://localhost:3001');
     setSocket(newSocket);
 
-    // Navigator mediaDevices'tan kullanıcı izni almak
     navigator.mediaDevices.getUserMedia({ video: true, audio: true })
       .then((currentStream) => {
         setStream(currentStream);
@@ -34,18 +33,14 @@ const VideoChat = () => {
       });
 
     return () => {
-      // ComponentWillUnmount gibi, bileşen kaldırıldığında socket bağlantısını temizle
       newSocket.disconnect();
     };
   }, []);
 
   useEffect(() => {
-    // socket değiştiğinde ve me event'i geldiğinde çalışır
     if (socket) {
-      socket.on('me', (id) => {
-        setMe(id);
-      });
-      
+      socket.on('me', (id) => setMe(id));
+
       socket.on('callUser', ({ from, name: callerName, signal }) => {
         setCall({ isReceivingCall: true, from, name: callerName, signal });
       });
@@ -54,10 +49,7 @@ const VideoChat = () => {
 
   const answerCall = () => {
     setCallAccepted(true);
-
-    // Peer bağlantısını oluşturmak ve olayları dinlemeye başlamak
     const peer = new SimplePeer({ initiator: false, trickle: false, stream });
-    peer.signal(call.signal);
     peer.on('signal', (data) => {
       socket.emit('answerCall', { signal: data, to: call.from });
     });
@@ -65,18 +57,19 @@ const VideoChat = () => {
       userVideo.current.srcObject = currentStream;
     });
 
+    peer.signal(call.signal);
+
     connectionRef.current = peer;
   };
 
   const callUser = (id) => {
-    // Peer bağlantısını oluşturmak ve olayları dinlemeye başlamak
     const peer = new SimplePeer({ initiator: true, trickle: false, stream });
     peer.on('signal', (data) => {
-      socket.emit('callUser', { userToCall: id, signalData: data, from: me, name});
+      socket.emit('callUser', { userToCall: id, signalData: data, from: me, name });
     });
     peer.on('stream', (currentStream) => {
-      if (userVideo.current) { // Ensure userVideo.current is not null
-        userVideo.current.srcObject = currentStream; // Assign srcObject if ref is not null
+      if (userVideo.current) {
+        userVideo.current.srcObject = currentStream;
       }
     });
     socket.on('callAccepted', (signal) => {
@@ -89,20 +82,28 @@ const VideoChat = () => {
 
   const leaveCall = () => {
     setCallEnded(true);
-    
-    // Bağlantıyı sonlandırmak ve sayfayı yeniden yüklemek
-    connectionRef.current && connectionRef.current.destroy();
 
-    socket.emit('endCall');
-  };
+    connectionRef.current && connectionRef.current.destroy();
   
+    socket.emit('endCall');
+
+  };
   useEffect(() => {
     if (socket) {
       socket.on('callEnded', () => {
-        window.location.reload();
+        // Karşı tarafın video elementini kaldır
+        if (userVideo.current) {
+          userVideo.current.srcObject = null; // Video akışını temizle
+          userVideo.current.remove(); 
+          const nameElement = document.querySelector("#armut")// Video elementini kaldır
+          if (nameElement) {
+            nameElement.remove();
+          }
+        }
       });
     }
   }, [socket]);
+
   return (
     <>
       <div className="flex justify-center items-center">
@@ -121,16 +122,17 @@ const VideoChat = () => {
           </div>
         )}
         {callAccepted && !callEnded && (
-          <div className="border-2 border-black rounded-lg p-4 m-4">
-            <h5 >{call.name || 'Name'}</h5>
+          <div className="border-2 border-black rounded-lg p-4 m-4" id="armut">
+           <h5 >{call.name || 'Name'}</h5>
             <video playsInline ref={userVideo} autoPlay className="w-64 md:w-80" />
           </div>
         )}
+        {/* Options bileşenini burada yerleştiriyoruz */}
+        <Options stream={stream} />
         <div className="w-full md:w-3/4 lg:w-1/2 xl:w-1/3 mx-auto mt-8">
           <div className="p-8 border-2 border-black">
             <div className="flex flex-col" noValidate autoComplete="off">
               <div className="w-full">
-                {/* Account Info */}
                 <div className="p-4">
                   <h6 className="text-lg mb-2">Account Info</h6>
                   <input
@@ -141,13 +143,11 @@ const VideoChat = () => {
                     placeholder="Name"
                   />
                   <CopyToClipboard text={me} className="mt-4 bg-blue-500 rounded-md outline-none">
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none">
-                      {me}
+                    <button className="text-white focus:outline-none">
+                      Copy Your ID
                     </button>
                   </CopyToClipboard>
                 </div>
-
-                {/* Make a call */}
                 <div className="p-4">
                   <h6 className="text-lg mb-2">Make a call</h6>
                   <input
@@ -171,7 +171,7 @@ const VideoChat = () => {
             </div>
           </div>
         </div>
-      </div >
+      </div>
     </>
   );
 };
